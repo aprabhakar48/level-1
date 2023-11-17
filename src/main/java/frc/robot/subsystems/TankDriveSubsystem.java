a665f3db2ac8d0a67b704b3d335315cef7de614d
aprabhakar48 <151100117+aprabhakar48@users.noreply.github.com>
2023-11-16 19:13:53 -0600

    aaaaaaaaa
    aaaaaaaaaaaaaa what is talon

--- src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/TankDriveSubsystem.java
@@ -1,17 +1,22 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import frc.robot.components.GenericMotor;
+import frc.robot.components.TalonComponent;
 public class TankDriveSubsystem extends SubsystemBase{
-    // Declare left and right side motors here,
-    // but don't set them to a value here
+    private GenericMotor leftMotor;
+    private GenericMotor rightMotor;
 
     public TankDriveSubsystem() {
-        // In the constructor, now set the motors to a value
+        rightMotor = new TalonComponent(TankDriveConstants.DRIVE_RIGHT_MOTOR_ID, "Talon SRX");
+        leftMotor = new TalonComponent(TankDriveConstants.DRIVE_LEFT_MOTOR_ID, "Talon SRX");
     }
 
     // Now create two methods, one for each motor
     // These methods should set the percent
     // Output of the left and right motors respectively
+    public void setLeftSpeed (double targetOutput) {
+        leftMotor.setOutput(ExtendedMath.clamp(-0.5, 0.5))
+    }
     
 }
